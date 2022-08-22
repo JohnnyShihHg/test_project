6f3da614ce3edcce8d4c8acd1a35a0ff91cd1cf9
JohnnyShihHg <johnnyshis.1997@gmail.com>
2022-08-22 17:59:17 +0800

    [Johnny 2022/08/22 創建 Service 連接Controller]

---
+++ app/Services/ShowPaymentService.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Services;
+
+use App\Model\ProductModel;
+
+class ProductService 
+{
+
+    private $product;
+
+    public function __construct(ProductModel $ProductModel)
+    {
+        $this->product = $ProductModel;
+    }
+
+    public function GetProduct()
+    {
+        /* 理應實現邏輯後調用方法 */
+        return $this->product->show();
+        // return 'hello';
+
+    }
+}
